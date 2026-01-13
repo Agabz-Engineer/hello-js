495515959431f031610f6fb052ed9cecccec296f
Agaba John <agaba.john@ashesi.edu.gh>
2026-01-13 23:18:37 +0000

    First JavaScript commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Agaba Great John"
+const age = " 18 "
+const favourite_prog_language = "Python"
+
+console.log("My name is", name)
+console.log("I am ", age, "years old")
+console.log("My favourite programming language is", favourite_prog_language)
